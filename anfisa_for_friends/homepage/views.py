ef1b335876f4a9c95cbcc23015d4947994c43b75
Aleksei Stupnikov <alexei.stupnikov@mail.ru>
2024-10-14 15:53:34 +0300

    Before final project

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from ice_cream.models import IceCream
+from django.db.models import Q
 
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    ice_cream_list = IceCream.objects.select_related('category').filter(
+        category__is_published=True
+    )
+    # ice_cream_list = IceCream.objects.values('id', 'title', 'category__title')
+    # ice_cream_list = IceCream.objects.values(
+    #     'id', 'title', 'description'
+    # ).filter(
+    #     Q(is_published=True) & Q(is_on_main=True)
+    #     | (
+    #         Q(is_published=True)
+    #         & Q(title__contains='пломбир')
+    #     )
+    # ).order_by('title')[1:4]
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
